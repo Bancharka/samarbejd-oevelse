b5f8d2d028b8903ccc3e7991afa73870538a2f91
hjoerdiskrohn <hmkr58281@edu.ucl.dk>
2024-02-12 12:21:18 +0100

    Hej

--- slideshow.js
+++ slideshow.js
@@ -1,26 +1,26 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slide = 1;
+slidesShow(slide);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  slidesShow(slide += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  slidesShow(slide = n);
 }
 
-function showSlides(n) {
+function slidesShow(n) {
   let i;
   let slides = document.getElementsByClassName("krohnSlider");
   let dots = document.getElementsByClassName("prikker");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slide = 1}    
+  if (n < 1) {slide = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slide-1].style.display = "block";  
+  dots[slide-1].className += " active";
 }
