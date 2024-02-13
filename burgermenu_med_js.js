a9170ffbb28b9c6c8f8b2ba291504df39c294775
Vivijensen <vivien.sorn@gmail.com>
2024-02-13 10:47:51 +0100

    var til let

--- burgermenu_med_js.js
+++ burgermenu_med_js.js
@@ -1,4 +1,4 @@
-var menuOptions = [
+let menuOptions = [
   { title: "BODENDISPLAYS", url: "https://www.krohn-displays.de/produkte/bodendisplays/" },
   { title: "ETUIS", url: "https://www.krohn-displays.de/produkte/etuis/" },
   { title: "KUNSTSTOFFDISPLAYS", url: "https://www.krohn-displays.de/produkte/kunststoffdisplays/" },
@@ -9,11 +9,11 @@ var menuOptions = [
 ];
 
 function openNav() {
-  var sidenav = document.getElementById("mySidenav");
+  let sidenav = document.getElementById("mySidenav");
   sidenav.innerHTML = '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>';
   // løb igennem array og tilføj menuoptioner
   menuOptions.forEach(function(option) {
-      var link = document.createElement("a");
+      let link = document.createElement("a");
       link.href = option.url;
       link.textContent = option.title;
       sidenav.appendChild(link);
